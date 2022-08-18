93b2defd97fb51f01eb3a96b03b421e98a1275da
sukresh.thoughtworks <manda.sukresh@thoughtworks.com>
2022-08-18 11:32:51 +0530

    Change header title

--- src/components/Navbar/Navbar/index.js
+++ src/components/Navbar/Navbar/index.js
@@ -9,7 +9,7 @@ export default function Navbar() {
         <div className="container">
           <div>
             <Bookmark/>
-            <h1>To Do</h1>
+            <h1>To Doo</h1>
           </div>
           
         </div>
